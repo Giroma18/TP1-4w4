8e74ce1c899a06f4a325a51f6d12c6c2a33968ad
Giroux-Martone <e1469417@cmaisonneuve.qc.ca>
2025-04-01 09:36:19 -0400

    ajout de l'image palmier dans le dossier image

--- 404.php
+++ 404.php
@@ -11,12 +11,17 @@
     <section class="erreur__contenu"  style="background-image: url('<?php echo $erreur_background ?>'); background-repeat: no-repeat">
 
         <div class="erreur__block">
-            <h1 class="erreur__titre">Erreur 404</h1>
-            <p>OOPS ! Désolé la page est introuvable.</p>
+            <h1 class="erreur__titre">Oops, vous avez échoué sur l'île 404 !</h1>
 
-            <div>
-                <?php get_template_part('gabarit/icone-sociaux') ?>
-            </div>
+        </div>
+
+        <div class="erreur__block">
+            <p>Pas de panique, cher membre explorateur ! Vous avez dérivé un peu trop loin des destinations de rêve que notre club a soigneusement sélectionnées pour vous. Reprenez votre périple en cliquant sur 'Accueil' pour découvrir à nouveau nos voyages d’exception !</p>
+        </div>
+        
+        <button class="erreur__button">
+            Retour à l'accueil
+        </button>
             <div class="erreur__menu">
                 <?php wp_nav_menu(array(
                         "menu"=>"404",
@@ -24,7 +29,9 @@
                         "container_class"=>"erreur__menu"
                 ));?>
             </div>
-        </div>
+
+        <input class="erreur__rechercher" type="placeholder">
+        
     </section>
     <?php get_footer() ?>
 </body>
